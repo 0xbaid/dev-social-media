eaa3046baddea474494585ed10341940641d6bde
Obaid <obaidulhaq20@gmail.com>
2020-04-12 03:49:51 +0500

    Routed files with Express Router

--- server.js
+++ server.js
@@ -2,10 +2,40 @@ const express = require('express');
 const connectDB = require('./config/db');
 const app = express();
 
+
+// Connect Database
 connectDB();
 
 PORT = process.env.PORT || 5000;
 
-app.get('/', (req,res) => res.send('API is running'))
+app.get('/', (req, res) => res.send('API is running'))
+
+// Define routes
+app.use('/api/user', require('./routes/api/user'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/posts', require('./routes/api/posts'))
+app.use('/api/profile', require('./routes/api/profile'))
 
 app.listen(PORT, console.log(`Server started on ${PORT}`))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
