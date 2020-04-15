ada8860c8f3dc111211982cb27d65c14e8773324
Obaid <obaidulhaq20@gmail.com>
2020-04-16 02:21:10 +0500

    created routed to get all profiles by user ID

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -98,5 +98,44 @@ router.post(
     }
   }
 );
+// @route GET api/profile
+// @desc GET all user profile
+// @access public
+router.get('/', async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate('user', ['name', 'profile']);
+    res.json(profiles);
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).send('Server Error');
+  }
+});
+
+// @route get api/profile/user/user_id
+// @desc Get profile by user id
+// @access public
+router.get('/user/:user_id', async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id,
+    }).populate('user', ['name', 'avatar']);
+
+    if (!profile) {
+      return res
+        .status(400)
+        .json({ msg: 'There is no profile for this user.' });
+    }
+    res.json(profile);
+  } catch (error) {
+    console.error(error.message);
+
+    if ((error.kind = 'ObjectId')) {
+      return res
+        .status(400)
+        .json({ msg: 'There is no profile for this user.' });
+    }
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
