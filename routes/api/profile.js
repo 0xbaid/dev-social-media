ff1be1210d501479ae3ae78c072791df924faaef
Obaid <obaidulhaq20@gmail.com>
2020-04-15 23:38:27 +0500

    created route for create and update profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const auth = require('../../middleware/auth')
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
+const { check, validationResult } = require('express-validator');
 
 
 // @route GET api/profile/me
@@ -19,6 +20,38 @@ router.get('/me', auth, async (req, res) => {
     console.error(error.message);
     res.status(500).send('Server Error');
   }
+});
+
+// @route GET api/profile
+// @desc Create and update profile
+// @access private
+router.post('/', [ auth, [
+  check('status', 'Status is required').not().isEmpty(),
+  check('skills', 'Skills are required').not().isEmpty()
+]], async (req, res) => {
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+  
+
+  const {
+    company,
+    location,
+    website,
+    bio,
+    skills,
+    status,
+    githubusername,
+    youtube,
+    twitter,
+    instagram,
+    linkedin,
+    facebook
+  } = req.body;
+
+
 });
 
 module.exports = router;
