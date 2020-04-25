5a3d7201a134e95e99416c5aeef86a99320ed555
Obaid <obaidulhaq20@gmail.com>
2020-04-25 19:58:31 +0500

    implemented alert action call with alert component

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,10 +1,12 @@
 import React, { Fragment, useState } from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
+//Redux
 import { connect } from 'react-redux'
 import { setAlert } from '../../redux/alert/alert.action';
 
-const Register = (props) => {
+const Register = ({ setAlert }) => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -21,7 +23,7 @@ const Register = (props) => {
   const handleSubmit = async (event) => {
     event.preventDefault();
     if (password !== password2) {
-      props.setAlert('Passwords do not match', 'danger');
+      setAlert('Passwords do not match', 'danger');
     } else {
       console.log(formData);
     // We will regiter user using redux action for checking that registeration is working or not
@@ -105,6 +107,9 @@ const Register = (props) => {
   );
 };
 
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired
+};
 
 
-export default connect(null, {setAlert})(Register);
+export default connect(null, { setAlert })(Register);
--- client/src/redux/alert/alert.action.js
+++ client/src/redux/alert/alert.action.js
@@ -5,6 +5,10 @@ export const setAlert = (msg, alertType) => (dispatch) => {
   const id = uuidv4();
   dispatch({
     type: alertActionTypes.SET_ALERT,
-    payload: { msg, alertType, id }
+    payload: { msg, alertType, id },
   });
+  setTimeout(() => dispatch({
+    type:alertActionTypes.REMOVE_ALERT,
+    payload: id
+  }), 2500)
 };
